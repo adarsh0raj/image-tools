62b9ef958f97fcacefba60fc0f0d8316a68ffe58
adarsh <rajadarsh2602@gmail.com>
2024-11-02 16:34:09 +0530

    added gh-pages for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Removebg from './removebg/Removebg';
 import Home from './home/Home';
 
